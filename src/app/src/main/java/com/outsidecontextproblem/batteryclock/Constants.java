0a1a0731d2053d65e1021c40b38dd3f13200de5c
Stevö <stevehuwjohn@gmail.com>
2022-10-09 19:30:21 +0100

    WIP

---
+++ src/app/src/main/java/com/outsidecontextproblem/batteryclock/Constants.java
@@ -0,0 +1,11 @@
+package com.outsidecontextproblem.batteryclock;
+
+public class Constants {
+    public static int BitmapDimensions = 500;
+    public static int BitmapCenter = 250;
+    public static int BackgroundRadius = 250;
+    public static int FrameRadius = 230;
+    public static int ArcCenterMin = 20;
+    public static int ArcCenterMax = 480;
+
+}
