db800ef41fe18643bcbc39eb388e67fd54da7022
Stevö <stevehuwjohn@gmail.com>
2022-10-11 13:18:01 +0100

    WIP

--- src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockWidgetConfigureActivity.java
+++ src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockWidgetConfigureActivity.java
@@ -98,8 +98,8 @@ public class BatteryClockWidgetConfigureActivity extends Activity {
             return;
         }
 
-        Intent serviceIntent = new Intent(this, BatteryClockWidgetService.class);
-        startService(serviceIntent);
+//        Intent serviceIntent = new Intent(this, BatteryClockWidgetService.class);
+//        startService(serviceIntent);
 
         mAppWidgetText.setText(loadTitlePref(BatteryClockWidgetConfigureActivity.this, mAppWidgetId));
     }
