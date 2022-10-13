39923aff3f6e7e1dba3d085d7e361f304bf2c5c1
Stevö <stevehuwjohn@gmail.com>
2022-10-13 15:34:46 +0100

    WIP

---
+++ src/app/src/main/java/com/outsidecontextproblem/batteryclock/Settings.java
@@ -0,0 +1,30 @@
+package com.outsidecontextproblem.batteryclock;
+
+import android.content.Context;
+
+public class Settings {
+
+    private static final String BATTERY_INDICATOR = "BatteryIndicator";
+
+    private ElementSettings _batteryLevelIndicatorSettings;
+
+    private int _appWidgetId;
+
+    public ElementSettings getBatteryLevelIndicatorSettings() {
+        return _batteryLevelIndicatorSettings;
+    }
+
+    public Settings(int appWidgetId) {
+        _appWidgetId = appWidgetId;
+
+        _batteryLevelIndicatorSettings = new ElementSettings(_appWidgetId, Constants.BezelIndicator);
+    }
+
+    public void loadSettings(Context context) {
+        _batteryLevelIndicatorSettings.loadSettings(context, BATTERY_INDICATOR);
+    }
+
+    public void saveSettings(Context context) {
+        _batteryLevelIndicatorSettings.saveSettings(context, BATTERY_INDICATOR);
+    }
+}
