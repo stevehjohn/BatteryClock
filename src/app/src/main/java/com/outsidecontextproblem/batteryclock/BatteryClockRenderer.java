af990ec6ef5a1b1bea3d3fbcf7122f75536ed3e7
Stevö <stevehuwjohn@gmail.com>
2022-10-13 11:09:16 +0100

    WIP

---
+++ src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockRenderer.java
@@ -0,0 +1,119 @@
+package com.outsidecontextproblem.batteryclock;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import java.util.Calendar;
+
+public class BatteryClockRenderer {
+
+    private final Paint _arcPaint;
+    private final Paint _circlePaint;
+    private final Paint _minutePaint;
+    private final Paint _hourPaint;
+    private final Paint _dotPaint;
+    private final Paint _backgroundPaint;
+    private final Paint _minuteTrailPaint;
+    private final Paint _hourTrailPaint;
+    private final Paint _dayArcPaint;
+
+    public BatteryClockRenderer() {
+
+        _arcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _arcPaint.setARGB(255, 107, 99, 255);
+        _arcPaint.setStyle(Paint.Style.STROKE);
+        _arcPaint.setStrokeWidth(Constants.BezelIndicator);
+
+        _circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _circlePaint.setARGB(64, 255, 255, 255);
+        _circlePaint.setStyle(Paint.Style.STROKE);
+        _circlePaint.setStrokeWidth(Constants.BezelOutline);
+
+        _minutePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _minutePaint.setARGB(255, 255, 255, 255);
+        _minutePaint.setStyle(Paint.Style.STROKE);
+        _minutePaint.setStrokeWidth(Constants.MinuteHandThickness);
+
+        _hourPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _hourPaint.setARGB(255, 255, 255, 255);
+        _hourPaint.setStyle(Paint.Style.STROKE);
+        _hourPaint.setStrokeWidth(Constants.HourHandThickness);
+
+        _dotPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _dotPaint.setARGB(255, 255, 255, 255);
+        _dotPaint.setStyle(Paint.Style.STROKE);
+        _dotPaint.setStrokeWidth(Constants.TickThickness);
+
+        _backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _backgroundPaint.setARGB(192, 32, 32, 32);
+
+        _minuteTrailPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _minuteTrailPaint.setARGB(64, 255, 255, 255);
+        _minuteTrailPaint.setStyle(Paint.Style.STROKE);
+        _minuteTrailPaint.setStrokeWidth(Constants.BezelOutline);
+
+        _hourTrailPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _hourTrailPaint.setARGB(64, 255, 255, 255);
+        _hourTrailPaint.setStyle(Paint.Style.STROKE);
+        _hourTrailPaint.setStrokeWidth(Constants.BezelOutline);
+
+        _dayArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        _dayArcPaint.setARGB(64, 255, 255, 255);
+    }
+
+    public Bitmap render(int level) {
+
+        Bitmap bitmap = Bitmap.createBitmap(Constants.BitmapDimensions, Constants.BitmapDimensions, Bitmap.Config.ARGB_8888);
+        bitmap.eraseColor(Color.TRANSPARENT);
+        Canvas canvas = new Canvas(bitmap);
+
+        canvas.drawCircle(Constants.BitmapCenter, Constants.BitmapCenter, Constants.BackgroundRadius, _backgroundPaint);
+
+        canvas.drawCircle(Constants.BitmapCenter, Constants.BitmapCenter, Constants.FrameRadius, _circlePaint);
+
+        int batteryArcOffset = Constants.BitmapCenter - Constants.BatteryIndicatorRadius;
+
+        canvas.drawArc(batteryArcOffset, batteryArcOffset, Constants.BitmapDimensions - batteryArcOffset, Constants.BitmapDimensions - batteryArcOffset, 270, (int) -(level * 3.6), false, _arcPaint);
+
+        for (int i = 0; i < 12; i++) {
+            float dotRadians = (float) ((float) ((i * 30) * (Math.PI / 180)) - Math.PI / 2);
+
+            canvas.drawLine((float) (Constants.BitmapCenter + Math.cos(dotRadians) * Constants.TickStart), (float) (Constants.BitmapCenter + Math.sin(dotRadians) * Constants.TickStart),
+                    (float) (Constants.BitmapCenter + Math.cos(dotRadians) * Constants.TickEnd), (float) (Constants.BitmapCenter + Math.sin(dotRadians) * Constants.TickEnd), _dotPaint);
+        }
+
+        int dayArcOffset = Constants.BitmapCenter - Constants.DayArcRadius;
+
+        float degreesForDay = 360F / 7F;
+
+        float dayDegrees = (((Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 2) + 7) % 7) * degreesForDay;
+
+        dayDegrees += (degreesForDay / 24) * Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+
+        canvas.drawArc(dayArcOffset, dayArcOffset, Constants.BitmapDimensions - dayArcOffset, Constants.BitmapDimensions - dayArcOffset, 270, dayDegrees, true, _dayArcPaint);
+
+        float minuteDegrees = Calendar.getInstance().get(Calendar.MINUTE) * 6;
+
+        int minuteArcOffset = Constants.BitmapCenter - Constants.MinuteHandLength;
+
+        canvas.drawArc(minuteArcOffset, minuteArcOffset, Constants.BitmapDimensions - minuteArcOffset, Constants.BitmapDimensions - minuteArcOffset, 270, minuteDegrees, false, _minuteTrailPaint);
+
+        float minuteRadians = (float) ((float) ((Calendar.getInstance().get(Calendar.MINUTE) * 6) * (Math.PI / 180)) - Math.PI / 2);
+
+        float hourDegrees = Calendar.getInstance().get(Calendar.HOUR) * 30 + (Calendar.getInstance().get(Calendar.MINUTE) / 2F);
+
+        int hourArcOffset = Constants.BitmapCenter - Constants.HourHandLength;
+
+        canvas.drawArc(hourArcOffset, hourArcOffset, Constants.BitmapDimensions - hourArcOffset, Constants.BitmapDimensions - hourArcOffset, 270, hourDegrees, false, _hourTrailPaint);
+
+        canvas.drawLine(Constants.BitmapCenter, Constants.BitmapCenter, (float) (Constants.BitmapCenter + Math.cos(minuteRadians) * Constants.MinuteHandLength), (float) (Constants.BitmapCenter + Math.sin(minuteRadians) * Constants.MinuteHandLength), _minutePaint);
+
+        float hourRadians = (float) ((float) ((Calendar.getInstance().get(Calendar.HOUR) * 30 + Calendar.getInstance().get(Calendar.MINUTE) / 2) * (Math.PI / 180)) - Math.PI / 2);
+
+        canvas.drawLine(Constants.BitmapCenter, Constants.BitmapCenter, (float) (Constants.BitmapCenter + Math.cos(hourRadians) * Constants.HourHandLength), (float) (Constants.BitmapCenter + Math.sin(hourRadians) * Constants.HourHandLength), _hourPaint);
+
+        return bitmap;
+    }
+}
--- src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockWidget.java
+++ src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockWidget.java
@@ -5,73 +5,20 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.hardware.display.DisplayManager;
 import android.os.BatteryManager;
 import android.util.Log;
 import android.view.Display;
 import android.widget.RemoteViews;
 
-import java.util.Calendar;
-
 public class BatteryClockWidget extends AppWidgetProvider {
 
-    private final Paint _arcPaint;
-    private final Paint _circlePaint;
-    private final Paint _minutePaint;
-    private final Paint _hourPaint;
-    private final Paint _dotPaint;
-    private final Paint _backgroundPaint;
-    private final Paint _minuteTrailPaint;
-    private final Paint _hourTrailPaint;
-    private final Paint _dayArcPaint;
+    private final BatteryClockRenderer _batteryClockRenderer;
 
     private DisplayManager _displayManager;
 
     public BatteryClockWidget() {
-
-        _arcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _arcPaint.setARGB(255, 107, 99, 255);
-        _arcPaint.setStyle(Paint.Style.STROKE);
-        _arcPaint.setStrokeWidth(Constants.BezelIndicator);
-
-        _circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _circlePaint.setARGB(64, 255, 255, 255);
-        _circlePaint.setStyle(Paint.Style.STROKE);
-        _circlePaint.setStrokeWidth(Constants.BezelOutline);
-
-        _minutePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _minutePaint.setARGB(255, 255, 255, 255);
-        _minutePaint.setStyle(Paint.Style.STROKE);
-        _minutePaint.setStrokeWidth(Constants.MinuteHandThickness);
-
-        _hourPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _hourPaint.setARGB(255, 255, 255, 255);
-        _hourPaint.setStyle(Paint.Style.STROKE);
-        _hourPaint.setStrokeWidth(Constants.HourHandThickness);
-
-        _dotPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _dotPaint.setARGB(255, 255, 255, 255);
-        _dotPaint.setStyle(Paint.Style.STROKE);
-        _dotPaint.setStrokeWidth(Constants.TickThickness);
-
-        _backgroundPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _backgroundPaint.setARGB(192, 32, 32, 32);
-
-        _minuteTrailPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _minuteTrailPaint.setARGB(64, 255, 255, 255);
-        _minuteTrailPaint.setStyle(Paint.Style.STROKE);
-        _minuteTrailPaint.setStrokeWidth(Constants.BezelOutline);
-
-        _hourTrailPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _hourTrailPaint.setARGB(64, 255, 255, 255);
-        _hourTrailPaint.setStyle(Paint.Style.STROKE);
-        _hourTrailPaint.setStrokeWidth(Constants.BezelOutline);
-
-        _dayArcPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        _dayArcPaint.setARGB(64, 255, 255, 255);
+        _batteryClockRenderer = new BatteryClockRenderer();
     }
 
     private void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
@@ -146,57 +93,10 @@ public class BatteryClockWidget extends AppWidgetProvider {
             return;
         }
 
-        Bitmap bitmap = Bitmap.createBitmap(Constants.BitmapDimensions, Constants.BitmapDimensions, Bitmap.Config.ARGB_8888);
-        bitmap.eraseColor(Color.TRANSPARENT);
-        Canvas canvas = new Canvas(bitmap);
-
-        canvas.drawCircle(Constants.BitmapCenter, Constants.BitmapCenter, Constants.BackgroundRadius, _backgroundPaint);
-
-        canvas.drawCircle(Constants.BitmapCenter, Constants.BitmapCenter, Constants.FrameRadius, _circlePaint);
-
         BatteryManager batteryManager = (BatteryManager) context.getSystemService(Context.BATTERY_SERVICE);
         int level = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
 
-        int batteryArcOffset = Constants.BitmapCenter - Constants.BatteryIndicatorRadius;
-
-        canvas.drawArc(batteryArcOffset, batteryArcOffset, Constants.BitmapDimensions - batteryArcOffset, Constants.BitmapDimensions - batteryArcOffset, 270, (int) -(level * 3.6), false, _arcPaint);
-
-        for (int i = 0; i < 12; i++) {
-            float dotRadians = (float) ((float) ((i * 30) * (Math.PI / 180)) - Math.PI / 2);
-
-            canvas.drawLine((float) (Constants.BitmapCenter + Math.cos(dotRadians) * Constants.TickStart), (float) (Constants.BitmapCenter + Math.sin(dotRadians) * Constants.TickStart),
-                    (float) (Constants.BitmapCenter + Math.cos(dotRadians) * Constants.TickEnd), (float) (Constants.BitmapCenter + Math.sin(dotRadians) * Constants.TickEnd), _dotPaint);
-        }
-
-        int dayArcOffset = Constants.BitmapCenter - Constants.DayArcRadius;
-
-        float degreesForDay = 360F / 7F;
-
-        float dayDegrees = (((Calendar.getInstance().get(Calendar.DAY_OF_WEEK) - 2) + 7) % 7) * degreesForDay;
-
-        dayDegrees += (degreesForDay / 24) * Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
-
-        canvas.drawArc(dayArcOffset, dayArcOffset, Constants.BitmapDimensions - dayArcOffset, Constants.BitmapDimensions - dayArcOffset, 270, dayDegrees, true, _dayArcPaint);
-
-        float minuteDegrees = Calendar.getInstance().get(Calendar.MINUTE) * 6;
-
-        int minuteArcOffset = Constants.BitmapCenter - Constants.MinuteHandLength;
-
-        canvas.drawArc(minuteArcOffset, minuteArcOffset, Constants.BitmapDimensions - minuteArcOffset, Constants.BitmapDimensions - minuteArcOffset, 270, minuteDegrees, false, _minuteTrailPaint);
-
-        float minuteRadians = (float) ((float) ((Calendar.getInstance().get(Calendar.MINUTE) * 6) * (Math.PI / 180)) - Math.PI / 2);
-
-        float hourDegrees = Calendar.getInstance().get(Calendar.HOUR) * 30 + (Calendar.getInstance().get(Calendar.MINUTE) / 2F);
-
-        int hourArcOffset = Constants.BitmapCenter - Constants.HourHandLength;
-
-        canvas.drawArc(hourArcOffset, hourArcOffset, Constants.BitmapDimensions - hourArcOffset, Constants.BitmapDimensions - hourArcOffset, 270, hourDegrees, false, _hourTrailPaint);
-
-        canvas.drawLine(Constants.BitmapCenter, Constants.BitmapCenter, (float) (Constants.BitmapCenter + Math.cos(minuteRadians) * Constants.MinuteHandLength), (float) (Constants.BitmapCenter + Math.sin(minuteRadians) * Constants.MinuteHandLength), _minutePaint);
-
-        float hourRadians = (float) ((float) ((Calendar.getInstance().get(Calendar.HOUR) * 30 + Calendar.getInstance().get(Calendar.MINUTE) / 2) * (Math.PI / 180)) - Math.PI / 2);
-
-        canvas.drawLine(Constants.BitmapCenter, Constants.BitmapCenter, (float) (Constants.BitmapCenter + Math.cos(hourRadians) * Constants.HourHandLength), (float) (Constants.BitmapCenter + Math.sin(hourRadians) * Constants.HourHandLength), _hourPaint);
+        Bitmap bitmap = _batteryClockRenderer.render(level);
 
         views.setImageViewBitmap(R.id.imageView, bitmap);
 
