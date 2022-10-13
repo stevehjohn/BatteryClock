ee3302e384c55d4d8ff6a82f76e5071d0399e638
Stevö <stevehuwjohn@gmail.com>
2022-10-13 14:24:59 +0100

    WIP

--- src/app/src/main/java/com/outsidecontextproblem/batteryclock/ClockElementConfigurator.java
+++ src/app/src/main/java/com/outsidecontextproblem/batteryclock/ClockElementConfigurator.java
@@ -1,33 +1,37 @@
 package com.outsidecontextproblem.batteryclock;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
 public class ClockElementConfigurator extends LinearLayout {
 
-    public ClockElementConfigurator(Context context) {
-        super(context);
-
-        initialize(context);
-    }
-
     public ClockElementConfigurator(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
 
-        initialize(context);
+        initialize(context, attrs);
     }
 
     public ClockElementConfigurator(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
 
-        initialize(context);
+        initialize(context, attrs);
     }
 
-    private void initialize(Context context) {
+    private void initialize(Context context, @Nullable AttributeSet attrs) {
         LayoutInflater.from(context).inflate(R.layout.clock_element_configurator_view, this);
+
+        TypedArray x = context.obtainStyledAttributes(attrs, R.styleable.ClockElementConfigurator);
+
+        String elementTitle = x.getString(R.styleable.ClockElementConfigurator_elementTitle);
+
+        TextView textElementTitle = findViewById(R.id.textElementTitle);
+
+        textElementTitle.setText(elementTitle);
     }
 }
