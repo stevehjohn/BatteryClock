b243170a564028037e480981c0fa27f5e068836d
Stevö <stevehuwjohn@gmail.com>
2022-10-14 16:45:50 +0100

    WIP

--- src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockRenderer.java
+++ src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockRenderer.java
@@ -5,7 +5,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Typeface;
-import android.util.Log;
 
 public class BatteryClockRenderer {
 
