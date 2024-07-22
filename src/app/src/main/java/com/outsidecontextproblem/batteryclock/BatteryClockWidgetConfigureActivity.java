4e4cc295716af32ddb22dd471aedd02a933a3bda
Stevo John <stevehuwjohn@gmail.com>
2024-07-22 16:36:29 +0100

    WIP

--- src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockWidgetConfigureActivity.java
+++ src/app/src/main/java/com/outsidecontextproblem/batteryclock/BatteryClockWidgetConfigureActivity.java
@@ -222,7 +222,7 @@ public class BatteryClockWidgetConfigureActivity extends Activity implements Run
 
         updatePreview();
 
-        _handler.postDelayed(this, 1_000);
+        _handler.postDelayed(this, 166);
     }
 
     @Override
@@ -231,7 +231,7 @@ public class BatteryClockWidgetConfigureActivity extends Activity implements Run
 
         updatePreview();
 
-        _handler.postDelayed(this, 1_000);
+        _handler.postDelayed(this, 166);
     }
 
     private void configureTimezones(Context context) {
